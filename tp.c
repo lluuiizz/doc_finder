dce163bfb682d608cd51c53517ef7a461756e620
Luiz Guilherme Silva <luiz.developer.acc@gmail.com>
2025-04-01 21:32:49 -0300

    Testes

---
+++ tp.c
@@ -0,0 +1,16 @@
+#include "indice_invertido.h"
+#include "lista.h"
+
+
+int main(void){
+
+	IndiceInvertido *indice = aloca_indice(6);
+
+	insere_documento(indice, (Registro){"prog.doc", {"algoritmo", "selecao"}, 2});
+	insere_documento(indice, (Registro){"aeds1.doc", {"algoritmo", "estrutura", "dados"}, 3});
+	insere_documento(indice, (Registro){"darwin.doc", {"selecao", "natural"}, 2});
+	consulta(indice, (Registro){"", {"selecao", "natural"}, 2});
+
+	libera_indice(indice);
+
+}
